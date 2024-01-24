b921795b4c81d717316679e3bd0473060391d840
orange <chbii@nate.com>
2024-01-24 15:10:44 +0900

    얍

--- tt/src/tt/test.java
+++
@@ -1,9 +0,0 @@
-package tt;
-
-public class test {
-	public static void main(String[] args) {
-		System.out.println("박수호 must die");
-		System.out.println("박수호 누가 죽임? 범인 찾아라");
-		System.out.println("런링맨시작");
-	}
-}
